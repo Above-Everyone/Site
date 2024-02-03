8b29eb2697136b55e008636a299a2bc950dfc32f
jguy2 <143593651+jguy2@users.noreply.github.com>
2024-02-04 01:33:46 +1100

    Update find_profile.php

--- find_profile.php
+++ find_profile.php
@@ -196,16 +196,6 @@ table, th, td {
                     N/A
                 </p>
             </div>
-            <div class="col-auto my-auto">
-                <div class="h-100">
-                    <h5 class="mb-1">
-                        Verified
-                    </h5>
-                    <p class="mb-0 font-weight-bold text-sm">
-                        N/A
-                    </p>
-                </div>
-            </div>
         </div>
     </a>
 </li>
